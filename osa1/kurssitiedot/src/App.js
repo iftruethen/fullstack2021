de2e33c11c8e02b0d64000dd3cf079cb80dc058c
iftruedo <jere.ahkiola@gmail.com>
2021-11-22 17:47:39 +0200

    osa 1.1 tehty (korjattu)

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -5,9 +5,17 @@ const Header = (props) => (
 )
 
 const Content = (props) => (
-  <p>
-    {props.name} {props.exercises}
-  </p>
+  <>
+    <p>
+      {props.name1} {props.exercises1}
+    </p>
+    <p>
+      {props.name2} {props.exercises2}
+    </p>
+    <p>
+      {props.name3} {props.exercises3}
+    </p>
+  </>
 )
 
 const Total = (props) => (
@@ -28,9 +36,7 @@ const App = () => {
   return (
     <div>
       <Header name={course} />
-      <Content name={part1} exercises={exercises1} />
-      <Content name={part2} exercises={exercises2} />
-      <Content name={part3} exercises={exercises3} />
+      <Content name1={part1} name2={part2} name3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total total={exercises1+exercises2+exercises3} />
     </div>
   )

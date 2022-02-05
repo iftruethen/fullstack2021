c602cc68f16313501c0c408aac79badbbb3594c3
iftruedo <jere.ahkiola@gmail.com>
2022-02-06 00:14:34 +0200

    osa 2.11 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = ({persons,searchTerm}) => persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}</p>)
 
@@ -22,15 +23,16 @@ const PersonForm = ({addNew,newName,newNumber,handleChange,handeNumberchange}) =
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+
+  console.log(persons)
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(data => setPersons(data.data))
+  },[])
+  console.log(persons)
 
   const addNew = (event) => {
     event.preventDefault()
